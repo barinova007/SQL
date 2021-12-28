6e28c8077b21cbd97861243d7a7d37700d7281d7
Lena Barinova <spilnaya@ukr.net>
2021-12-28 22:26:09 +0200

    hm_2

---
+++ HW_2/Barinova Script_hm_2.sql
@@ -0,0 +1,546 @@
+--==========================================
+--Таблица employees
+--1)Создать таблицу employees
+-- id. serial,  primary key,
+--- employee_name. Varchar(50), not null
+create table employees (
+id serial primary key,
+employee_name varchar(50) not null
+);
+--проверить содержимое
+select * from employees;
+
+--2)Наполнить таблицу employee 70 строками.
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+insert into employees (id, employee_name)
+values (default, 'Lena Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Oksana Barinova'); 
+
+insert into employees (id, employee_name)
+values (default, 'Valeriy Barinov'); 
+
+--проверить содержимое
+select * from employees;
+
+--==========================================
+--Таблица salary
+--1) Создать таблицу salary
+-- id. Serial  primary key,
+-- monthly_salary. Int, not null
+create table salary (
+id serial primary key,
+monthly_salary int not null
+);
+--проверить содержимое
+select * from salary;
+
+--2)Наполнить таблицу salary 15/16 строками:
+insert into salary (id, monthly_salary)
+values (default, '1000'); 
+
+insert into salary (id, monthly_salary)
+values (default, '1100'); 
+
+insert into salary (id, monthly_salary)
+values (default, '1200'); 
+
+insert into salary (id, monthly_salary)
+values (default, '1300');
+
+insert into salary (id, monthly_salary)
+values (default, '1400'); 
+
+insert into salary (id, monthly_salary)
+values (default, '1500'); 
+
+insert into salary (id, monthly_salary)
+values (default, '1600'); 
+
+insert into salary (id, monthly_salary)
+values (default, '1700'); 
+
+insert into salary (id, monthly_salary)
+values (default, '1800'); 
+
+insert into salary (id, monthly_salary)
+values (default, '1900'); 
+
+insert into salary (id, monthly_salary)
+values (default, '2000');
+
+insert into salary (id, monthly_salary)
+values (default, '2100'); 
+
+insert into salary (id, monthly_salary)
+values (default, '2200'); 
+
+insert into salary (id, monthly_salary)
+values (default, '2300'); 
+
+insert into salary (id, monthly_salary)
+values (default, '2400'); 
+
+insert into salary (id, monthly_salary)
+values (default, '2500'); 
+--проверить содержимое
+select * from salary;
+--==========================================
+Таблица employee_salary
+
+--5)Создать таблицу employee_salary
+-- id. Serial  primary key,
+-- employee_id. Int, not null, unique
+-- salary_id. Int, not null
+
+create table employee_salary(
+id serial primary key,
+employee_id int not null unique,
+salary_id Int not null
+);   
+
+--проверить содержимое
+select * from employee_salary;
+--6)Наполнить таблицу employee_salary 40 строками:
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  5 ,  1 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  6 ,  2 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  7 ,  3 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  8 ,  4 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  9 ,  5 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  10 ,  6 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  11 ,  7 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  12 ,  8 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  13 ,  9 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  14 ,  10 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  15 ,  11 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  16 ,  12 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  17 ,  13 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  18 ,  14 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  19 ,  15 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  20 ,  16 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  21 ,  1 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  22 ,  2 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  23 ,  3 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  24 ,  4 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  25 ,  5 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  26 ,  6 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  27 ,  7 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  28 ,  8 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  29 ,  9 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  30 ,  10 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  31 ,  11 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  32 ,  12 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  33 ,  13 );
+-- в 10 строк из 40 вставить несуществующие employee_id
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  310 ,  14 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  309 ,  15 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  308 ,  16 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  307,  1 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  306,  2 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  305,  3 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  304,  4 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  303,  5 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  302,  6 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  301,  7 );
+insert into employee_salary (id, employee_id, salary_id)
+values (default,  300 ,  8 );
+
+--========================================================
+--Таблица roles
+--7) Создать таблицу roles
+-- id. Serial  primary key,
+-- role_name. int, not null, unique
+create table roles(
+id serial  primary key,
+role_name int not null unique
+);
+--проверить содержимое
+select * from roles;
+--8) Поменять тип столба role_name с int на varchar(30)
+alter table roles
+alter column role_name type varchar(30);
+---если с varchar на int:
+--alter table roles
+--alter column role_name type int;
+
+--9) Наполнить таблицу roles 20 строками:
+insert into roles (id, role_name)
+values (default, 'Junior Python developer');
+insert into roles (id, role_name)
+values (default, 'Middle Python developer');
+insert into roles (id, role_name)
+values (default, 'Senior Python developer');
+insert into roles (id, role_name)
+values (default, 'Junior Java developer');
+insert into roles (id, role_name)
+values (default, 'Middle Java developer');
+insert into roles (id, role_name)
+values (default, 'Senior Java developer');
+insert into roles (id, role_name)
+values (default, 'Junior JavaScript developer');
+insert into roles (id, role_name)
+values (default, 'Middle JavaScript developer');
+insert into roles (id, role_name)
+values (default, 'Senior JavaScript developer');
+insert into roles (id, role_name)
+values (default, 'Junior Manual QA engineer');
+insert into roles (id, role_name)
+values (default, 'Middle Manual QA engineer');
+insert into roles (id, role_name)
+values (default, 'Senior Manual QA engineer');
+insert into roles (id, role_name)
+values (default, 'Project Manager');
+insert into roles (id, role_name)
+values (default, 'Designer');
+insert into roles (id, role_name)
+values (default, 'HR');
+insert into roles (id, role_name)
+values (default, 'CEO');
+insert into roles (id, role_name)
+values (default, 'Sales manager');
+insert into roles (id, role_name)
+values (default, 'Junior Automation QA engineer');
+insert into roles (id, role_name)
+values (default, 'Middle Automation QA engineer');
+insert into roles (id, role_name)
+values (default, 'Senior Automation QA engineer');
+
+--====================================================
+--Таблица roles_employee
+--10)Создать таблицу roles_employee
+-- id. Serial  primary key,
+-- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+-- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+
+create table roles_employee (
+id serial primary key,
+employee_id int not null unique,
+role_id int not null,
+foreign key (employee_id)
+references employees (id),
+foreign key (role_id)
+references roles (id)
+);
+
+select*from roles_employee;
+--11)Наполнить таблицу roles_employee 40 строками:
+insert into roles_employee (id, employee_id, role_id)
+values (default, 5, 3 );
+insert into roles_employee (id, employee_id, role_id)
+values (default, 6, 11 );
+insert into roles_employee (id, employee_id, role_id)
+values (default, 45, 1);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 65, 2);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 34, 3);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 46, 6);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 54, 8);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 23, 4);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 43, 10);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 42, 11);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 11, 13);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 25, 14);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 7, 11);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 8, 8);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 9, 9);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 10, 6);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 15, 1);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 12, 2);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 13, 4);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 16, 6);
+insert into roles_employee (id, employee_id, role_id)
+values (default, 17, 8);
+insert into roles_employee (id,employee_id, role_id)
+values (default, 18, 1);
+insert into roles_employee (id,employee_id, role_id)
+values (default, 19, 2 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 20, 3 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 21, 4 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 22, 5 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 44, 6 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 24, 7 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 26, 8 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 27, 9 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 28, 10 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 29, 11 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 30, 12 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 31, 13 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 32, 14);
+insert into roles_employee (id,employee_id, role_id)
+values (default, 33, 15);
+insert into roles_employee (id,employee_id, role_id)
+values (default, 37, 16 );
+insert into roles_employee (id,employee_id, role_id)
+values (default, 70, 1);
+insert into roles_employee (id,employee_id, role_id)
+values (default, 68, 2);
+insert into roles_employee (id,employee_id, role_id)
+values (default, 69, 3);
